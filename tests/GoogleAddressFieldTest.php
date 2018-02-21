62bce69c6d613858e2960026e508bafcac1f025f
sunnysideup <sunnysideupdevs@gmail.com>
2018-02-21 16:40:18 +1300

    PATCH: module clean-up

--- tests/GoogleAddressFieldTest.php
+++ tests/GoogleAddressFieldTest.php
@@ -1,13 +1,12 @@
 <?php
-class GoogleAddressFieldTest extends SapphireTest {
+class GoogleAddressFieldTest extends SapphireTest
+{
+    protected $usesDatabase = false;
 
-	protected $usesDatabase = false;
+    protected $requiredExtensions = array();
 
-	protected $requiredExtensions = array();
-
-	public function testMyMethod() {
-		$this->assertEquals(1, 1);
-	}
-
+    public function testMyMethod()
+    {
+        $this->assertEquals(1, 1);
+    }
 }
-
